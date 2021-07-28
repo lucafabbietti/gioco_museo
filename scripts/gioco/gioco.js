aaf8b1a79c8fc02ceac3124455e217e3ecbcfa22
lucafabbietti <lucafabbietti@yahoo.it>
2021-07-28 17:47:32 +0200

    json customization

--- scripts/gioco/gioco.js
+++ scripts/gioco/gioco.js
@@ -7,10 +7,13 @@ $(document).ready(function(){
         $("#paragrafo2").hide();
         $("#paragrafo1").show();
     });
-    var interventi = $.getJSON("../assets/interventi.json");
-    interventi.forEach(i => {
-        console.log(i.id);
-        var riga = document.createElement("td");
-        //var cella_codice = 
-    });
+
+    $.getJSON("../../assets/gioco/interventi.json", function(result){
+        result.forEach(element => {
+            console.log(element.id);
+        });
+    }).fail(function(){console.log("errore")});
+
+
+
 });
