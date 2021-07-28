fe8cc11040bacfe813d6097105406ca576e8dd9e
Giacomo Vecchi <77987323+SlicedStream@users.noreply.github.com>
2021-07-28 19:53:48 +0200

    Upload 19:53

--- scripts/scriptMappa.js
+++ scripts/scriptMappa.js
@@ -1,25 +1,35 @@
 
+// FUNZIONE CONTEGGIO //
+var i = 0;
+
+  function contatore() {
+        i++
+        console.log(i);
+    }
+// -------- //
 
 
-var s=0;
 
-function contatore() {
-    s=s+1;
 
-  console.log(s);
+
+function avanti() {
+     
     
-    switch (s) {
+}
+    switch (i) {
         case 1:
-            document.getElementById('p1').style.display='inline';
+            document.getElementById('p1').style.display = 'none';
+            document.getElementById('p2').style.display = 'inline';
             break;
-            case 2:
-                document.getElementById('p1').style.display='none';
-        document.getElementById('p2').style.display='inline';
-                
-                break;
-    
-        default: 
+        case 2:
+            document.getElementById('p2').style.display = 'none';
+            document.getElementById('p3').style.display = 'inline';
+            break;
+
+        default:
             break;
     }
-}
+
+ 
+
 
