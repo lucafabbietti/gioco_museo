2ca34e84d7dec1b965c677e9ac1cf2cf32d706e5
lucafabbietti <lucafabbietti@yahoo.it>
2021-07-28 14:40:18 +0200

    aaa

--- scripts/gioco.js
+++ scripts/gioco.js
@@ -7,4 +7,10 @@ $(document).ready(function(){
         $("#paragrafo2").hide();
         $("#paragrafo1").show();
     });
+    var interventi = $.getJSON("../assets/interventi.json");
+    interventi.forEach(i => {
+        console.log(i.id);
+        var riga = document.createElement("td");
+        //var cella_codice = 
+    });
 });
