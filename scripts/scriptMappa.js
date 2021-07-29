2c27468abe966dcae821a668e9cdabbc5202abd3
Giacomo Vecchi <77987323+SlicedStream@users.noreply.github.com>
2021-07-29 15:51:51 +0200

    Upload 15:59

--- scripts/scriptMappa.js
+++ scripts/scriptMappa.js
@@ -1,35 +1,50 @@
 
-// FUNZIONE CONTEGGIO //
 var i = 0;
 
-  function contatore() {
-        i++
-        console.log(i);
+
+// FUNZIONE CONTEGGIO AVANTI //
+
+function clickAvanti() {
+    if (i < 2) {
+        i++;
     }
+
+    cambiaPagina();
+}
+// -------- //
+
+// FUNZIONE CONTEGGIO INDIETRO //
+
+
+function clickIndietro() {
+    if (i > 0) {
+        i--;
+    }
+    cambiaPagina();
+}
 // -------- //
 
 
+function cambiaPagina() {
+    console.log("dentro avanti " + i);
+document.getElementById('indietro').style.display = 'none';
+document.getElementById('avanti').style.display = 'inline';
 
-
-
-function avanti() {
-     
-    
-}
     switch (i) {
+        case 0:
+            document.getElementById('p1').style.display = 'inline';
+            break;
+
         case 1:
             document.getElementById('p1').style.display = 'none';
             document.getElementById('p2').style.display = 'inline';
+            document.getElementById('indietro').style.display = 'inline';
             break;
         case 2:
             document.getElementById('p2').style.display = 'none';
             document.getElementById('p3').style.display = 'inline';
-            break;
-
-        default:
+            document.getElementById('avanti').style.display = 'none';
+            document.getElementById('indietro').style.display = 'inline';
             break;
     }
-
- 
-
-
+}
