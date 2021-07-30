7546b6e8193ec3b41841e65f5a0dfdee9a235092
lucafabbietti <lucafabbietti@yahoo.it>
2021-07-30 12:56:13 +0200

    added button and load events json

--- scripts/gioco/gioco.js
+++ scripts/gioco/gioco.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
     //caricamento interventi da file json e aggiunta di righe sulla tabella
 
     var interventi = "";
+    var eventi = "";
+
+    var costo_max = 200;
+
     $.getJSON("../../assets/gioco/interventi.json", function(result){
         interventi = result;
-        console.log("-----"+interventi[0].id);
         result.forEach(element => {
             
             var row = "";
@@ -15,24 +18,27 @@ $(document).ready(function(){
 
             //aggiungo la checkbox
             row += "<td> <input type=\"checkbox\" class=\"check\" id=\"check_"+ element.id +"\"> </td>";
-            console.log(element.id);
+            //console.log(element.id);
 
             //aggiungo il tag dell'intervento
             row += "<td>"+ element.codice_intervento +"</td>"
-            console.log(element.codice_intervento);
+            //console.log(element.codice_intervento);
 
             //aggiungo la descrizione dell'intervento
             row += "<td>"+ element.descrizione +"</td>"
-            console.log(element.descrizione);
+            //console.log(element.descrizione);
 
             //aggiungo il costo dell'intervento
             row += "<td>"+ element.costo +"</td>"
-            console.log(element.costo);
+            //console.log(element.costo);
 
-            console.log(row);
-
+            //console.log(row);
+            
+            //aggiungo la riga alla tabella
             $("#table").append(row);       
         });
+
+        //aggiungo l'evento click a tutte le checkbox
 
         $(".check").click(function() {
             var costo = 0;
@@ -48,16 +54,57 @@ $(document).ready(function(){
                 }
             }
 
+            //se il costo totale è superiore a 80000, comunico all'utente l'errore,
+            //tutte le checkbox diventano unchecked e azzero il costo.
+            if(costo > costo_max)
+            {
+                alert("Hai superato il budget di 80000! Seleziona gli interventi con più attenzione!");
+                $(".check").prop( "checked", false );
+                costo = 0;
+            }
+
             //mostro il prezzo attuale sulla casella di testo
             $("#costo_totale").text(costo);
 
+        });
+
+        //aggiungo l'evento click al bottone di start
+        $("#start_btn").click(function() {
+
+            //se non ci sono errori nella lettura del json eventi, inizio la simulazione
+            if(!carica_eventi())
+            {
+                //nascondo la tabella
+                $("#table_master").hide();
+            
+                simulazione();
+            }
+            
 
         });
 
 
+
+
     }).fail(function(){console.log("errore sulla lettura di interventi.json")});
 
-    
+    function carica_eventi()
+    {
+        $.getJSON("../../assets/gioco/eventi.json", function(result){
+
+
+            return true;
+        }).fail(function() 
+        {
+            alert("errore sulla lettura di eventi.json");
+            return false;
+        });
+    }
+
+    function simulazione()
+    {
+        alert("bona raga");
+    }
 
 
 
